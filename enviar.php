99b2fe7b0ab6883c67db73adfad332d82e610a58
Carlos Xavier <carlos.lipe559@hotmail.com>
2023-08-14 16:55:33 -0300

    add page contato/ infomarcoes

---
+++ enviar.php
@@ -0,0 +1,33 @@
+<?php
+if (isset($_POST["email"]) && !empty(trim($_POST["email"])) && !empty($_POST["nome"]) && !empty($_POST["assunto"])) {
+
+    $nome = addslashes($_POST['nome']);
+    $email = addslashes($_POST['email']);
+    $telefone = addslashes($_POST['telefone']);
+    $assunto = addslashes($_POST["assunto"]);
+    $textarea = addslashes($_POST["mensagem"]);
+
+
+    $email_to = "geral@ideas4web.pt";
+
+    $header = "Content-Type: text/html; charset=utf-8" . "\r\n";
+    $header .= "From:" . $email . "\r\n" .
+        "Reply-To:" . $email . "\e\n" .
+        "X=Mailer:PHP/" . phpversion();
+
+    // Dados que serão enviados.
+    $body = "<strong>Nome: </strong>" . $nome . "</br>" .
+        "<strong>Email: </strong>" . $email . "</br>" .
+        "<strong>Telefone: </strong>" . $telefone . "</br>" .
+        "<strong>Mensagem: </strong>" . $textarea;
+
+    //Enviando o email.
+    $status = mail($email_to, $assunto, $body, $header);
+
+}
+if ($status) {
+    header("Location:index.php?status=sucesso");
+} else {
+    header("Location:index.php?status=erro");
+}
+?>
--- js/script.js
+++ js/script.js
@@ -21,3 +21,36 @@ function menu() {
 
 }
 menu();
+
+function teste() {
+    let mais = document.querySelectorAll('.mais');
+    let title = document.querySelectorAll('.sub_title_servicos');
+    let teste = document.querySelectorAll('.mais p');
+
+    for (let i = 0; i < mais.length; i++) {
+
+        mais[i].addEventListener("click", () => {
+
+            if (mais[i].classList.contains("close")) {
+                mais[i].classList.toggle("close");
+                title[i].classList.toggle("open");
+                teste[i].innerText = "+";
+            } else {
+                mais[i].classList.add("close");
+                title[i].classList.add("open");
+                teste[i].innerText = "-";
+
+            }
+
+        });
+
+    }
+
+}
+teste();
+/*if( mais[0].classList.contains("title1") ) {
+    title[0].style.height = 'auto';
+    
+}else {
+
+}*/
